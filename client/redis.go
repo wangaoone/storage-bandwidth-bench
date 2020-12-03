d0cc3f95b5a0ce417919ed8a3bef108e2ff4496c
wa <shenlan.wa@alibaba-inc.com>
2020-12-03 18:06:52 +0800

    added redis client

---
+++ client/redis.go
@@ -0,0 +1,29 @@
+package client
+
+import (
+	"github.com/go-redis/redis/v7"
+)
+
+type Redis struct {
+	client *redis.Client
+}
+
+func NewRedisClient(addr string) *Redis {
+	return &Redis{
+		client: redis.NewClient(&redis.Options{
+			Addr: addr,
+		}),
+	}
+}
+
+func (c *Redis) Get(key string) ([]byte, error) {
+	val, err := c.client.Get(key).Bytes()
+	if err != nil {
+		return nil, err
+	}
+	return val, nil
+}
+
+func (c *Redis) Set(key string, val []byte) error {
+	return c.client.Set(key, val, 0).Err()
+}
