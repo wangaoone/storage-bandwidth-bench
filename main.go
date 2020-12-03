3effaf847178140999d5bf90820705f91b1245a6
wa <shenlan.wa@alibaba-inc.com>
2020-12-03 10:08:54 +0800

    Support thread control

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"math/rand"
 	"os"
 	"storage-throughput-bench/client"
+	"sync"
 	"sync/atomic"
 	"time"
 )
@@ -20,10 +21,12 @@ var (
 	keyIdx     = flag.Int64("keyIdx", 0, "key start index")
 	fileName   = flag.String("o", "tempFile", "output file name")
 	bucketName = flag.String("bucket", "bandwidth.ao.test", "bucket name")
+	thread     = flag.Int("thread", 1, "number of concurrent thread")
 )
 
-func perform(duration time.Duration, thread int, s3 *client.S3) {
+func perform(duration time.Duration, threadIdx int, s3 *client.S3, wg *sync.WaitGroup) {
 	t := time.NewTicker(duration)
+	defer wg.Done()
 	defer t.Stop()
 	quitC := make(chan bool, 1)
 	go func() {
@@ -33,9 +36,10 @@ func perform(duration time.Duration, thread int, s3 *client.S3) {
 				return
 			default:
 				currentIdx := atomic.AddInt64(keyIdx, 1)
-				key := generateKey(currentIdx)
+				key := generateKey(currentIdx, threadIdx)
 				val := make([]byte, SIZE)
 				rand.Read(val)
+				fmt.Printf("thread is %v, key is %v\n", threadIdx, key)
 				start := time.Now()
 				err := s3.Set(key, val)
 				if err != nil {
@@ -54,25 +58,32 @@ func perform(duration time.Duration, thread int, s3 *client.S3) {
 	}
 }
 
-func generateKey(idx int64) string {
+func generateKey(idx int64, threadIdx int) string {
 	year, month, day := time.Now().Date()
 	hour, minute, second := time.Now().Clock()
-	return fmt.Sprintf("%v%v%v-%v%v%v-%v", year, int(month), day, hour, minute, second, idx)
+	return fmt.Sprintf("%v%v%v-%v%v%v-%v-%v", year, int(month), day, hour, minute, second, idx, threadIdx)
 }
 
 func main() {
+	flag.Parse()
+
 	f, err := os.OpenFile(*fileName, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
 	//set output of logs to f
 	log.SetOutput(f)
-
 	defer f.Close()
+
 	log.Printf("key, size, duration\n")
 	s3Client := client.NewS3Client(*bucketName)
+	var wg sync.WaitGroup
 	start := time.Now()
-	perform(BenchDuration, 1, s3Client)
+	for i := 0; i < *thread; i++ {
+		wg.Add(1)
+		go perform(BenchDuration, i, s3Client, &wg)
+	}
+	wg.Wait()
 	duration := time.Since(start)
 	log.Printf("Total latency is %v\n", duration.Seconds())
 }
